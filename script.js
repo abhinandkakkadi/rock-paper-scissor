5860576cf09589202d5b42b4034d21e3fb61617c
Abhinand KR <nanduttanvsabhi@gmail.com>
2022-11-25 10:36:44 +0530

    Add getComputerChoice funtion to generate random choice against user

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log(5);
+
+function getComputerChoice()
+{
+    const choice = ["rock" , "paper" , "scissor"];
+    let computerSelection = choice[Math.floor(Math.random() * choice.length)];
+    console.log(computerSelection);
+}
+
+getComputerChoice();
