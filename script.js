6b300ebe276bb24b00367d5022c896b9dc5e61b0
Abhinand KR <nanduttanvsabhi@gmail.com>
2022-11-30 23:54:34 +0530

    Add eventlistner to all the buttons for each choice

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 let compPoints = 0;
 let userPoints = 0;
 
+const result = document.getElementById('demo');
+
+
 function getComputerChoice()
 {
     const choice = ["rock" , "paper" , "scissor"];
@@ -18,27 +21,27 @@ function round(computerSelection,playerSelection)
    
    if(playerSelection === 'rock' && computerSelection === 'scissor' || playerSelection === 'scissor' && computerSelection === 'paper'
       || playerSelection === 'paper' && computerSelection === 'rock' )
-      {
-        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+      { console.log = `You win! ${playerSelection} beats ${computerSelection}`;
+        result.innerHTML =`You win! ${playerSelection} beats ${computerSelection}`;
         userPoints+=1;
       }
    if(playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'scissor' && computerSelection === 'rock'
       || playerSelection === 'paper' &&  computerSelection === 'scissor' )
-     {
-        console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+     {  console.log = `You lose! ${computerSelection} beats ${playerSelection}`;
+        result.innerHTML =`You lose! ${computerSelection} beats ${playerSelection}`;
         compPoints+=1;
      }
 
    if( playerSelection === 'rock' && computerSelection === 'rock' || playerSelection === 'scissor' && computerSelection === 'scissor'||
        playerSelection === 'paper' && computerSelection === 'paper' )
-    {
-        console.log(`Tie both are ${computerSelection}`);
+    {   console.log = `Tie both are ${computerSelection}`;
+        result.innerHTML =`Tie both are ${computerSelection}`;
     }
   
   
 }
 
-function game()
+/*function game()
 {
     for( let i=1; i<= 100; i++)
     {
@@ -59,4 +62,22 @@ function game()
         }
     }
 }
-game();
+game(); */
+
+
+// round(getComputerChoice(),rock.innerHTML)
+
+(document.getElementById("rock").addEventListener("click", function()
+{round(getComputerChoice(),rock.innerHTML)}))
+
+
+document.getElementById('paper').addEventListener('click',function(){
+    round(getComputerChoice(),paper.innerHTML)
+})
+
+document.getElementById('scissor').addEventListener('click',function(){
+    round(getComputerChoice(),scissor.innerHTML)
+})
+
+
+
